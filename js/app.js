2c0e1a444df6995046db9948bceb75a508a91d33
Winston Davis Mclean <winston.davis.mclean@gmail.com>
2016-08-17 18:05:06 -0400

    set up app.js, home index, and made some folders and working on controllers and insta index

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular
+    .module("wdinstagram", [
+      "ui.router",
+      "insta"
+  ])
+    .config([
+        "$stateProvider",
+        RouterFunction
+    ]);
+
+  // router
+  function RouterFunction($stateProvider){
+    $stateProvider
+      .state("instaIndex", {
+        url: "/instas",
+        templateUrl: "js/insta/index.html",
+        controller: "InstaIndexController",
+        controllerAs: "IIvm"
+      })
+  }
 })();
---
+++ js/insta/index.controller.js
@@ -0,0 +1,10 @@
+"use strict";
+
+(function(){
+  angular
+    .module("instas")
+    .contoller("InstaIndexController", [
+        InstaIndexControllerFunction
+    ]);
+  
+    
